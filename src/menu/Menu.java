8931a724e730bd6c357527ab644ca70d34eddf19
Lafliche <antoine.flichy@grenoble-inp.org>
2019-12-05 21:34:08 +0100

    menu2

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -10,27 +10,37 @@ import gui.GUI;
 
 public class Menu extends GUI {
 	
-	private int widthButton = 100;
+	private int widthButton = 120;
 	private int  heightButton = 30;
+	private int buttonGap= 40;
+	private int lastScore = 60;
 	
 	public Menu(){
 		super();
-		
-		addButton((width - widthButton) / 2, height/2, widthButton, heightButton, "New Game", "GAME");
-		addButton((width - widthButton) / 2, height/2 + 40, widthButton, heightButton, "Quit", "EXIT");
+		addButton((width - widthButton) / 2, height/2 + this.buttons.size()*this.buttonGap, widthButton, heightButton, "New Game", "GAME");
+		addButton((width - widthButton) / 2, height/2 + this.buttons.size()*this.buttonGap, widthButton, heightButton, "Highest Score", "SCORE");
+		addButton((width - widthButton) / 2, height/2 + this.buttons.size()*this.buttonGap, widthButton, heightButton, "Quit", "EXIT");
+	}
+	
+	public Menu(int lastScore){
+		this();
+		this.lastScore = lastScore;
 	}
 	
 	@Override
 	public void render(){
 		super.render();
 		writeCentered(fontMenu, "Menu", width / 2, height/3);
+		if (lastScore != -1){
+			writeCentered(fontMenu, "Dernier Score : " + this.lastScore, width / 2, height/3 + this.buttonGap);
+		}
 	}
 	
 	@Override
 	public void onClick(){
 		super.onClick();
 		switch(this.pressedButton){
-			case 1:
+			case 2:
 				System.exit(1);
 				break;
 		}
